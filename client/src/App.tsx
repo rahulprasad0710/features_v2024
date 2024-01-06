6b3a275a45eeeb96466ab220d5aa6fe278ca16f4
rahulprasad0710 <raulshah2021@hotmail.com>
2024-01-06 23:16:33 +0545

    wip: chat app connected

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { io, Socket } from "socket.io-client";
+import Chatbox from "./components/Chatbox";
+import { ClientToServerEvents, ServerToClientEvents } from "./types/socket";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [newMessage, setNewMessage] = useState<string>("");
+    const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+        "http://localhost:3000",
+        {
+            withCredentials: true,
+            extraHeaders: {
+                "my-custom-header": "abcd",
+            },
+        }
+    );
+    useEffect(() => {
+        if (!socket) return;
+        socket.on("connect", () => {
+            console.log("connected");
+        });
+    }, [socket]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        if (!socket) return;
+        socket.on("msg-from-server", (data) => {
+            console.log(data);
+        });
+    }, [socket]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        socket.emit("chat-message", newMessage);
+    };
+
+    return (
+        <div>
+            {/* <h1 className='text-3xl font-bold underline'>Hello world!</h1> */}
+            {/* <Chatbox socket={socket} /> */}
+            <footer className='bg-white border-t border-gray-300 p-4 absolute bottom-0 w-3/4'>
+                <div className='flex items-center'>
+                    <input
+                        type='text'
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                        placeholder='Type a message...'
+                        className='w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500'
+                    />
+                    <button
+                        type='submit'
+                        onClick={(e) => handleSubmit(e)}
+                        className='bg-indigo-500 text-white px-4 py-2 rounded-md ml-2'>
+                        Send
+                    </button>
+                </div>
+            </footer>
+        </div>
+    );
 }
 
-export default App
+export default App;
