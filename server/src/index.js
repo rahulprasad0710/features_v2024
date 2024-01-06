c19432e466102f885d12330835352a3e25bae43f
rahulprasad0710 <raulshah2021@hotmail.com>
2024-01-06 23:17:25 +0545

    wip: chat app coneected

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const httpServer = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        credentials: true,
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("chat-message", (msg) => {
+        console.log("recieved message: " + msg);
+        socket.emit("msg-from-server", {
+            message: `msg from server: ${msg}`,
+            username: socket.username,
+        });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.status(200).json("Hello World!");
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
